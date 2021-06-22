9852725c3a90eb5f3cb8fd8678817b67019b98aa
Yossi <dvoryy@gmail.com>
2021-06-22 23:02:31 +0300

    K-Means update + Sample file

--- k_means.py
+++ k_means.py
@@ -5,75 +5,91 @@ import matplotlib.pyplot as plt
 def plot_list(list):
     centers = []
     dots = []
-    # height, weight and country data
-    height = [167, 175, 170, 186, 190, 188, 158, 169, 183, 180]
-    weight = [65, 70, 72, 80, 86, 94, 50, 58, 78, 85]
-    country = ['A', 'A', 'B', 'B', 'B', 'B', 'A', 'A', 'B', 'A']
-
-    # color map for each category
-    colors = {'A': 'orange', 'B': 'blue'}
-    color_ls = [colors[i] for i in country]
 
     # plot
     for element in list:
         centers.append(element[0])
         dots.append(element[1])
-    #colors_ls =
-    plt.scatter(dots,centers)
-    plt.xlabel("Weight (Kg)")
-    plt.ylabel("Height (cm)")
-    plt.title("Height v/s Weight")
+    result = dots + centers
+
+    plt.scatter(centers,dots,color='red')
     plt.show()
 
-ans = []
+
 def read_data_files(name):
-   clustering_dataset = pd.read_csv("Clustering.csv")
-   #print(clustering_dataset.head(10))
-   #print ("Read data files successfully")
+    return pd.read_csv("sample.csv")
+  # return pd.read_csv("Clustering.csv")
 
-   #print (clustering_dataset['Machine.num.1'])
-   centers = pick_start_points(3, clustering_dataset['Machine.num.1'])
+def update_centers(list):
+    ans = 0.0
+    new_centers = []
+    i=0
+    for element in list:
+        ans=sum(element)/len(element)
 
-   for dot in clustering_dataset['Machine.num.1']:
+        new_centers.append(ans)
+    print (new_centers)
+    return new_centers
+
+
+def update_diff(list):
+     ans = 0.0
+     new_centers = []
+     i = 0
+     for element in list:
+         ans += ((sum(element)-(centers[i]*len(element)))**2)/len(element)
+         i+=1
+
+         print("this is the diff:", ans )
+         new_centers.append(ans)
+     return (sum(new_centers)/len(new_centers))
+
+
+def classify(dots_list):
+   ans = []
+   res = []
+   total = []
+   for dot in dots_list:
        min_dist = 1000000
        current_center = 0
        for center in centers:
-            if dot==center:
-                continue
             current_dist=abs(dot-center)
             if current_dist < min_dist:
                 min_dist = current_dist
                 current_center = center
        ans.append((current_center,dot))
-
    #print (ans)
-   plot_list(ans)
+   for center in centers:
+       res = []
+       for item in ans:
+            if (center == item[0]):
+                res.append(item[1])
+                #print (res)
+       total.append(res)
 
-
-
-#class k_means_c:
- #   def __init__(self):
-        #self.data = clustering_dataset
-        #self.centers = pick_start_points()
-
+   #print (total)
+   return total
 
 def pick_start_points(num_of_centers, x_array):
     return random.sample(list(x_array), num_of_centers)
 
 
-   # def find_cluster_centers(self):
-   #     return
-
-    #def find_clusters(self):
-    #    pick_start_points(3)
-
-#class dbscan_c:
-#    def __init__(self):
-     #   self.data=NULL
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print("starting to read data files")
-    read_data_files('self')
-  #  clustering_dataset.head(10)
+    clustering_dataset = read_data_files('self')
+    d_in_prog =  clustering_dataset['Machine.num.1']
+    K=5
+
+    old_diff = 20000.0
+    new_diff = 10000.0
+    centers = pick_start_points(K, d_in_prog)
+    while old_diff - new_diff > 1 :
+        old_diff=new_diff
+        ans = classify(d_in_prog)
+        new_diff = update_diff(ans)
+        print (new_diff)
+        centers = update_centers(ans)
+
+    plot_list(ans)
+
