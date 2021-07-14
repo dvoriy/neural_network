b233eeaa7a78a1d5b2c3cd3c4ac6d81e6c29ba0b
Roi101 <roiyaacovi@gmail.com >
2021-07-15 00:30:11 +0300

    started na handling

--- neural_project.py
+++ neural_project.py
@@ -26,25 +26,27 @@ import numpy as np
 # 10. normalize the data
 # 11. balance the data
 # 12. use random forest information gain in order to determine which features ae more important
+# 13. create confusion matrix and valuation indicators: precision, recall, accuracy, auc
+# 14. integrate mode and median
+# 15. determine buy_premium as target variable
+# 16. buy_premium has NA values handle it
 
 def date_loading(path):
     """loads the data need to get a path"""
 
-def data_splitting(data):
+# def data_splitting(data):
 
-
-def data_engineering(data):
+def feature_engineering(data):
     """receives the project data and cleans the data"""
     data['Gender'].replace("F", 1, inplace=True)  # replace F to 1
     data['Gender'].replace("M", 0, inplace=True)  # replace M to 0 # unsure if needed can it work with m and f?
-
     print(train_dataset.head(10)) # in order to verify
 
 def drop_unneeded_columns(data):
-    """drop unneeded columns: user_Id, Unnamed: 0, ought_premium"""
-    data = data.drop(columns="User_ID")  # dropping the user_Id columns
-    data = data.drop(columns="Unnamed: 0")  # dropping the Unnamed: 0 columns
-    data = data.drop(columns="Bought_premium")  # dropping the Bought_premium columns
+    """drop unneeded columns(features): user_Id, Unnamed: 0, ought_premium"""
+    data = data.drop(columns="User_ID")  # dropping the user_Id column. 0 correlation with all of the features and has no meaning.
+    data = data.drop(columns="Unnamed: 0")  # dropping the Unnamed: 0 column. 0 correlation with all of the features, it seems that it comes from the csv numbering
+    data = data.drop(columns="Bought_premium")  # dropping the Bought_premium column there is no need for him the same as Buy_premium
 
     print(train_dataset.head(10))  # in order to verify
 
@@ -79,11 +81,20 @@ def plot_cor_matrix(data):
     return sorted_mat1
 
 # functions to handle Na values
-def print_how_many_na_col(data):
+def how_many_na_col(data):
+    """get the number of missing data per column"""
+    missing_values_count = data.isnull().sum()
+    missing_values_count.append(data.isnull().sum() * 100 / len(data))
+    return missing_values_count[0:]
 
-def print_how_many_na_row(data):
+def how_many_na_col_precent(data):
+    """get the precent of missing data per column"""
+    missing_values_count = (data.isnull().sum() * 100 / len(data))
+    return missing_values_count[0:]
 
-def elemention_function():
+#def print_how_many_na_row(data):
+
+#def elemention_function():
 
 # Press the green button in the gutter to run the script.
 # if __name__ == '__main__':
@@ -93,23 +104,26 @@ def elemention_function():
 train_dataset = pd.read_csv("ctr_dataset_train.csv")
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.max_rows', 500)
-print(train_dataset.head(10))
-print_data_unique_values(train_dataset)
-print_data_summaries(train_dataset)
+# print(train_dataset.head(10))
+print(how_many_na_col(train_dataset))
+print(how_many_na_col_precent(train_dataset))
+# print_data_unique_values(train_dataset)
+# print_data_summaries(train_dataset)
+plot_cor_matrix(train_dataset)
 
 
 # print(train_dataset[["Gender", "Location", "Date", "Time", "Min_prod_time", "Max_prod_time", "Commercial_1", "Commercial_2",
 #                "Commercial_3", "Mouse_activity_1", "Mouse_activity_2", "Mouse_activity_3", "Jewelry", "Shoes", "Clothing",
 #                "Home", "Premium", "Premium_commercial_play", "Idle", "Post_premium_commercial", "Size_variations",
 #                "Color_variations", "Dispatch_loc", "Buy_premium"]].describe(include="all", datetime_is_numeric=True)) # show the dataset 5 statistics
-print(train_dataset[["Gender", "Location", "Date", "Time", "Min_prod_time", "Max_prod_time", "Commercial_1", "Commercial_2",
-               "Commercial_3", "Mouse_activity_1", "Mouse_activity_2", "Mouse_activity_3", "Jewelry", "Shoes", "Clothing",
-               "Home", "Premium", "Premium_commercial_play", "Idle", "Post_premium_commercial", "Size_variations",
-               "Color_variations", "Dispatch_loc", "Buy_premium"]].median()) # shows median
-print(train_dataset[["Gender", "Location", "Date", "Time", "Min_prod_time", "Max_prod_time", "Commercial_1", "Commercial_2",
-               "Commercial_3", "Mouse_activity_1", "Mouse_activity_2", "Mouse_activity_3", "Jewelry", "Shoes", "Clothing",
-               "Home", "Premium", "Premium_commercial_play", "Idle", "Post_premium_commercial", "Size_variations",
-               "Color_variations", "Dispatch_loc", "Buy_premium"]].mode()) # shows mode
+#print(train_dataset[["Gender", "Location", "Date", "Time", "Min_prod_time", "Max_prod_time", "Commercial_1", "Commercial_2",
+               # "Commercial_3", "Mouse_activity_1", "Mouse_activity_2", "Mouse_activity_3", "Jewelry", "Shoes", "Clothing",
+               # "Home", "Premium", "Premium_commercial_play", "Idle", "Post_premium_commercial", "Size_variations",
+               # "Color_variations", "Dispatch_loc", "Buy_premium"]].median()) # shows median
+#print(train_dataset[["Gender", "Location", "Date", "Time", "Min_prod_time", "Max_prod_time", "Commercial_1", "Commercial_2",
+               # "Commercial_3", "Mouse_activity_1", "Mouse_activity_2", "Mouse_activity_3", "Jewelry", "Shoes", "Clothing",
+               # "Home", "Premium", "Premium_commercial_play", "Idle", "Post_premium_commercial", "Size_variations",
+               # "Color_variations", "Dispatch_loc", "Buy_premium"]].mode()) # shows mode
 
 
 
