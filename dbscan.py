d036cb24a22c41676019b1c65284b439a1d3a768
Yossi <dvoryy@gmail.com>
2021-07-17 22:50:20 +0300

    update silhuette width + code cleanup Adding the project requirements doc

--- dbscan.py
+++ dbscan.py
@@ -61,28 +61,6 @@ def find_neighbors(element, list):
             neighbors.append(neighbor)
     return neighbors
 
-def density_connected(element,k,list):
-    neighbors = find_neighbors(element, list)
-    for neighbor in neighbors:
-        index.update({neighbor: k})
-        if neighbor in core:
-            if index[neighbor] == -1:
-                density_connected(neighbor, k, list)
-
-
-
-def scanner(list):
-    noise = []
-    for element in list:
-        index.update({element : -1})
-        if is_core(element , list):
-            core.append(element)
-    k = 0
-    for element in core:
-        if index[element] == -1:
-            k = k+1
-            index.update({element : k})
-            density_connected(element,k,list)
 
 def add_neighbors_to_core(element,core,list):
     visited.append(element)
@@ -106,29 +84,8 @@ def recursive_dbscan(list):
             clusters.append(core)
 
 
-def align_clusters(index):
-    cluster = []
-    created_clusters = []
-    for element in index:
-        if index[element] in created_clusters:
-            continue
-        if index[element] == -1:
-            continue
-        cluster = []
-        created_clusters.append(index[element])
-        cluster.append(element)
-        for neighbor in index:
-            if element == neighbor:
-                continue
-            if index[neighbor] == index[element]:
-                cluster.append(neighbor)
-        clusters.append((cluster))
- #       print (clusters)
-                #index.pop(neighbor)
-
 
 def plot_list(list):
-    #data = list
     x = []
     y = []
     color = []
@@ -174,12 +131,7 @@ if __name__ == '__main__':
 
             ts = time.time()
             recursive_dbscan (dataset)
-          #  scanner(dataset)
-          #  align_clusters(index)
-            #ts1 = time.time()
-     #       centers = update_centers(clusters)
-     #       print (centers)
-     #       print (update_diff(clusters,centers))
+
             s_w_temp = silhuette_width(clusters)
             print("s_w_temp is " + str(s_w_temp))
             if s_w_temp < s_w:
