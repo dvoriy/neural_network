aa5ccc998509e495388cba9341bc3f0e6544920c
Yossi <dvoryy@gmail.com>
2021-07-21 22:27:12 +0300

    SKL API update

--- SKL_DBSCAN.py
+++ SKL_DBSCAN.py
@@ -1,6 +1,8 @@
 from sklearn.cluster import DBSCAN
 import pandas as pd
 import numpy as np
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 
 # https://scikit-learn.org/stable/auto_examples/cluster/plot_dbscan.html#sphx-glr-auto-examples-cluster-plot-dbscan-py
 
@@ -22,3 +24,23 @@ if __name__ == '__main__':
         n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
         n_noise_ = list(labels).count(-1)
         print(n_clusters_)
+
+
+    pca = PCA(n_components=2)
+    X_principal = pca.fit_transform(clustering_dataset)
+    X_principal = pd.DataFrame(X_principal)
+    X_principal.columns = ['P1', 'P2']
+    cs = []
+    results = []
+    for i in range(1, K):
+        clustering = DBSCAN(eps=4.0/i, min_samples=10, n_jobs=-1).fit(X_principal)
+        core_samples_mask = np.zeros_like(clustering.labels_, dtype=bool)
+        core_samples_mask[clustering.core_sample_indices_] = True
+        labels = clustering.labels_
+        n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+        n_noise_ = list(labels).count(-1)
+        print(n_clusters_)
+
+    plt.scatter(X_principal['P1'], X_principal['P2'],
+           c = DBSCAN(eps=4.0/5, min_samples=10, n_jobs=-1).fit(X_principal), cmap =plt.cm.winter)
+    plt.show()
--- SKL_KMEANS.py
+++ SKL_KMEANS.py
@@ -1,6 +1,7 @@
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.decomposition import PCA
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -30,3 +31,33 @@ if __name__ == '__main__':
     plt.ylabel('CS')
     plt.show()
 
+    pca = PCA(n_components=2)
+    X_principal = pca.fit_transform(clustering_dataset)
+    X_principal = pd.DataFrame(X_principal)
+    X_principal.columns = ['P1', 'P2']
+
+
+    cs = []
+    results = []
+    for i in range(1, K):
+        kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
+        kmeans.fit(X_principal)
+        cs.append(kmeans.inertia_)
+        results.append(kmeans.cluster_centers_)
+
+    best_num_of_clusters =0
+    print(cs)
+    for i in range(K):
+        if cs[i] / cs[i + 1] < 1.2:
+            print(results[i])
+            best_num_of_clusters = i
+            break
+
+    plt.plot(range(1, K), cs)
+    plt.title('The Elbow Method')
+    plt.xlabel('Number of clusters')
+    plt.ylabel('CS')
+    plt.show()
+    plt.scatter(X_principal['P1'], X_principal['P2'],
+           c = KMeans(n_clusters = best_num_of_clusters).fit_predict(X_principal), cmap =plt.cm.winter)
+    plt.show()
